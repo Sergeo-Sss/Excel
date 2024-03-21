ca2ddb2df5d7b133b0b6eb29294fe541675b7f9d
Sergey Antonov <59932798+Sergeo-Sss@users.noreply.github.com>
2024-03-21 16:24:34 +0300

    Initial commit

---
+++ Excel/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Office.Interop.Excel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Excel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Application excelApp = new Application();
+            excelApp.Visible = false;
+
+            Workbook workbook = excelApp.Workbooks.Open(args[0]);
+            excelApp.Run("Update"); // Запускаем макрос
+
+            // Сохраняем и закрываем файл дубликата таблицы расчета Excel, сама таблица расчета не сохраняется
+            File.Delete(args[1]);
+            workbook.SaveAs (args[1], Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            workbook.Close(false);
+            excelApp.Quit();
+
+            //Освобождаем ресурсы
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+        }
+    }
+}
